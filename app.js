6866b4ee350e9dc21d553bee75a971146a35d8ce
Martin Kennedy <kennedym103@gmail.com>
2016-12-06 21:20:57 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 function addListItem() {
    var text = $('#newItem').val();
    if ($('#newItem').val() === '') {} else {
-      $('#list').append('<li>' + ' <button  class="done"></button> ' + text + '  <button class="delete" >X</button>' + '</li>');
+      $('#list').append('<li>' + ' <button  class="done"></button><span> ' + text + '</span>  <button class="delete" >X</button>' + '</li>');
       localStorage.setItem("todolist", $('#list').html());
       $('#newItem').val('')
    }
 }
 
 function finishItem() {
-   if ($(this).parent().css('background') === 'linear-gradient(to bottom, #656565, #4e4e4e)'){
-      $(this).parent().css({'background': '#3d3d3d', 'border-top': '2px solid #535353'});
-   } else if ($(this).parent().css('background') ===  '#3d3d3d') {
-         $(this).parent().css({'background': 'linear-gradient(to bottom, #656565, #4e4e4e)', 'border-top': '2px solid #6a6a6a'})
-   }
+      $(this).parent().toggleClass('done-checked');
+      localStorage.setItem("todolist", $('#list').html());
 }
 
 $(document).ready(function () {
