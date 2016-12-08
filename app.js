e0ca16d150bbe0880588804ba6d47d639923cb47
Martin Kennedy <kennedym103@gmail.com>
2016-12-08 10:42:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+// add list item from the input and append item into html
 function addListItem() {
    var text = $('#newItem').val();
    if ($('#newItem').val() === '') {} else {
@@ -5,29 +8,40 @@ function addListItem() {
       localStorage.setItem("todolist", $('#list').html());
       $('#newItem').val('')
    }
+
 }
+
+//toggle checked button on and off
 
 function finishItem() {
       $(this).parent().toggleClass('done-checked');
       localStorage.setItem("todolist", $('#list').html());
 }
 
+
 $(document).ready(function () {
 
+
    $('#addItem').on('click', addListItem);
+   $('#newTask').keypress(function(e){
+        if(e.which == 13){//Enter key pressed
+            $('#addItem').click();
+        }
+    });
 
    $(document).on("click", ".delete", function () {
       $(this).parent().remove();
       localStorage.setItem("todolist", $('#list').html());
    });
    $(document).on("click", ".done", finishItem);
+
 });
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+   $('.addBtn').addClass('.addBtn-mobile');
+}
+
 
 $(function () {
    $('#list').html(localStorage.getItem("todolist"));
 });
-
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $('.addBtn').toggleClass('addBtn-mobile');
-}
